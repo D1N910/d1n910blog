9f22d3bd38f82555e0b14d27f36ac1e7a09dd796
D1N910 <wyD1n910@gmail.com>
2018-07-24 22:41:13 +0800

    添加了个进入后台的入口。

--- blogpcweb/src/components/Index.vue
+++ blogpcweb/src/components/Index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="index">
     <div class="logo">
-      <img src="https://wx4.sinaimg.cn/mw690/006ES7aSgy1ftdctro1r2j30590590sn.jpg" alt="">
+      <img src="https://wx4.sinaimg.cn/mw690/006ES7aSgy1ftdctro1r2j30590590sn.jpg" alt="" @click="jumptoback">
+    </div>
+    <div v-if='jump>=3'>
+      <input type="text" placeholder="在这里输入神秘的密码" v-model="daima">
+      <div @click="innerbg">进入后台</div>
+      <div @click="innerbg">取消</div>
     </div>
     <h1>{{ msg }}</h1>
     <h2>根据下面的链接跟踪我</h2>
@@ -51,7 +56,35 @@ export default {
   data() {
     return {
       msg: '这里是D1n910的blog',
+      jump: 0,
+      daima: '',
+      url: '',
     };
+  },
+  methods: {
+    jumptoback() {
+      this.jump += 1;
+      if (this.daima !== '') {
+        const url = [];
+        url.push('https://www.d1n910.cn/api/jumptobg?daima=');
+        url.push(this.daima);
+        const geturl = url.join('');
+        this.$http.get(
+          geturl,
+        ).then((response) => {
+          const pages = response.bodyText;
+          if (pages !== 'OK') {
+            window.location.href = JSON.parse(response.bodyText).ddd;
+          }
+        });
+        this.jump = 0;
+        this.daima = '';
+      }
+    },
+    innerbg() {
+      this.jump = 0;
+      this.daima = '';
+    },
   },
 };
 </script>
--- blogpcweb/src/router/index.js
+++ blogpcweb/src/router/index.js
@@ -12,6 +12,11 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'Index',
+      component: Index,
+    },
+    {
       path: '/index',
       name: 'Index',
       component: Index,
